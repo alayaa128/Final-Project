8d2a2af5dc5726ee4bef2aa902d5f00b2b448cca
Alaya Andres <alayaa128@gmail.com>
2026-04-12 19:26:57 -0500

    started classes

---
+++ src/project.py
@@ -0,0 +1,26 @@
+import pygame
+import random
+
+class Character:
+
+class Obstacle:
+
+class Obstacle_Rain:
+
+def main():
+    #create safe zone
+#game loop
+    #event loop
+    #when character gets hit with obstacle
+    #hurt sound effect
+    #new event loop
+    #When character hits safe zone
+    #winner sound effect
+    #display text
+    #background music
+
+    #quit
+    
+
+if __name__ == "__main__":
+    main()
