7747b994bb502b42a8b312aaa0adb2f4c74923f7
Alaya Andres <alayaa128@gmail.com>
2026-04-14 00:52:46 -0500

    coded character and obstacle class

--- src/project.py
+++ src/project.py
@@ -1,15 +1,51 @@
 import pygame
 import random
 
-class Character:
+class Character():
 
-class Obstacle:
+    def __init__(self, pos=(960,950), size=30):
+        self.pos = pos
+        self.size = size
+        self.color = pygame.Color(0, 255, 0)
+        self.alpha = 255
+    
+    def draw(self, surface):
+        pygame.draw.circle(surface, self.color, self.pos, self.size)
 
-class Obstacle_Rain:
+class Obstacle():
+
+    def __init__(self, pos=(100,70), size=random.randint(30, 40), life=1000):
+        self.pos = pos
+        self.size = size
+        self.color = pygame.Color(100, 0, 255)
+        self.alpha = 255
+    
+    def draw(self, surface):
+        pygame.draw.circle(surface, self.color, self.pos, self.size)
+
+#class Obstacle_Rain():
 
 def main():
+    #initialize game
+    pygame.init()
+    pygame.display.set_caption("Dodge or Die!")
+    resolution = (1920, 1080)
+    screen = pygame.display.set_mode(resolution)
+    obstacle = Obstacle()
+    character = Character()
+    running = True
     #create safe zone
-#game loop
+    #game loop
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        black = pygame.Color(0, 0, 0)
+        screen.fill(black)
+        obstacle.draw(screen)
+        character.draw(screen)
+        pygame.display.flip()
+    pygame.quit()
     #event loop
     #when character gets hit with obstacle
     #hurt sound effect
@@ -18,8 +54,6 @@ def main():
     #winner sound effect
     #display text
     #background music
-
-    #quit
     
 
 if __name__ == "__main__":
